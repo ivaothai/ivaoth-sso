d704234bc48eb4ea5a4cb830b92ad9f5ae812cd2
Sarun Intaralawan <sarun.intaralawan@ivao.aero>
2020-11-12 20:29:11 +0700

    feat: add `OAuthState` to entity list

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,38 @@
-import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
 import { CreateAuthRequestTable1557146563440 } from './migrations/1557146563440-CreateAuthRequestTable';
+import { Admin } from './entities/Admin';
 import { AuthRequest } from './entities/AuthRequest';
+import { OAuthState } from './entities/OAuthState';
 import { User } from './entities/User';
+import { APIKeyMiddleware } from './middlewares/APIKey.middleware';
+import { CreateAuthRequestTable1557146563440 } from './migrations/1557146563440-CreateAuthRequestTable';
 import { CreateUserTable1557153360741 } from './migrations/1557153360741-CreateUserTable';
 import { CreateNicknameOnUsers1557162358099 } from './migrations/1557162358099-CreateNicknameOnUsers';
-import { APIKeyMiddleware } from './middlewares/APIKey.middleware';
-import { Admin } from './entities/Admin';
 import { CreateAdminTable1588883056001 } from './migrations/1588883056001-CreateAdminTable';
+import { CreateOAuthStateTable1605167513355 } from './migrations/1605167513355-CreateOAuthStateTable';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mariadb',
       timezone: 'Z',
+      host: process.env['DB_HOST'],
       username: process.env['DB_USERNAME'],
       password: process.env['DB_PASSWORD'],
       database: process.env['DB_NAME'],
-      entities: [AuthRequest, User, Admin],
+      entities: [AuthRequest, User, Admin, OAuthState],
       migrations: [
         CreateAuthRequestTable1557146563440,
         CreateUserTable1557153360741,
         CreateNicknameOnUsers1557162358099,
-        CreateAdminTable1588883056001
+        CreateAdminTable1588883056001,
+        CreateOAuthStateTable1605167513355
       ],
       migrationsRun: true
     }),
-    TypeOrmModule.forFeature([AuthRequest, User, Admin])
+    TypeOrmModule.forFeature([AuthRequest, User, Admin, OAuthState]),
   ],
   controllers: [AppController],
   providers: []
