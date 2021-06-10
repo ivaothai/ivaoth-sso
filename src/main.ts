c13ed0993a89ebfb0896c0d8634b2fdf9687dd59
Sarun Intaralawan <sarun.intaralawan@ivao.aero>
2021-06-11 01:12:39 +0700

    feat(app): add rendering

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { resolve } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(resolve(__dirname, '..', 'public'));
+  app.setBaseViewsDir(resolve(__dirname, 'views'));
+  app.setViewEngine('ejs');
   await app.listen(process.env.PORT);
 }
 void bootstrap();
